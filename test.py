d7746c68c9b11baf34b5f0d4bf3f7a060b033488
achraf dardour <51725754+achrafder99@users.noreply.github.com>
2021-07-13 16:35:12 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import telnyx as tlnx
 
-numbers = input("Write  numbers :")
+file_name = input("file name :")
 
-list_of_numbers = open("list_numbers.txt","a")
-list_of_numbers.write(numbers)
+list_of_numbers = open(file_name + ".txt","r")
 
 def receiv_sms():
     
@@ -12,7 +11,6 @@ def receiv_sms():
     your_telnyx_number = "+15732046266"
 
     
-
     for list in list_of_numbers :
         destination_number = list
 
@@ -21,5 +19,3 @@ def receiv_sms():
         to = destination_number,
         text ="Hello, world!",
     )
-
-receiv_sms()
