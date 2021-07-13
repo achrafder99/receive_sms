90688b1dbe8c04f2f740eae0ee0da70e13c808bc
achraf dardour <51725754+achrafder99@users.noreply.github.com>
2021-07-13 16:25:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
 import telnyx as tlnx
 
-def recive_msg():
+numbers = input("Write  numbers :")
 
-    number = str(+212674309507)
-    to = "number "
+list_of_numbers = open("list_numbers.txt","a")
+list_of_numbers.write(numbers)
 
+def receiv_sms():
+    
     url_api = "KEY017A9CBE884E29EC5A630F6C64A56B95_NZnX5T8P8mrBXQSysI0vmU"
-    tlnx.api_key =  url_api
+    tlnx.api_key = url_api
+    your_telnyx_number = "+15732046266"
 
-    try:
-        tlnx.Message.create(
-        from_= number,
-        to= to,
-        msg = "heey",
+    
+
+    for list in list_of_numbers :
+        destination_number = list
+
+    tlnx.Message.create(
+        from_ =your_telnyx_number,
+        to = destination_number,
+        text ="Hello, world!",
     )
-    except:
-        print("Error Message")
 
-recive_msg()
+receiv_sms()
